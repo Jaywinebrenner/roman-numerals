521e9d1d59157194f470b481c9e43b8df43c6faa
Student <example@example.com>
2019-10-31 14:26:39 -0700

    add code to replace a character iterating four times with two characters as is proper in roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,75 @@
 // var numD
 function convertatron(num) {
 
-  for (var z = num; z >= 0; z--){
-    if (z >= 1000){
-      var m = parseInt(z / 1000);
-      z = (z % 1000);
+
+
+    if (num >= 4000 || (isNaN(num))){
+        return ("Enter a number between 1 and 3,999, please and thanks.");
     }
-    if (z >= 500){
-      var d = parseInt(z / 500);
-      z = (z % 500);
+    if (num >= 1000){
+      var m = parseInt(num / 1000);
+      num = (num % 1000);
     }
-    if (z >= 100){
-      var c = parseInt(z / 100);
-      z = (z % 100);
+    if (num >= 500){
+      var d = parseInt(num / 500);
+      num = (num % 500);
     }
-    if (z >= 50){
-      var l = parseInt(z / 50);
-      z = (z % 50);
+    if (num >= 100){
+      var c = parseInt(num / 100);
+      num = (num % 100);
     }
-    if (z >= 10){
-      var x = parseInt(z / 10);
-      z = (z % 10);
+    if (num >= 50){
+      var l = parseInt(num / 50);
+      num = (num % 50);
     }
-    if (z >= 5){
-      var v = parseInt(z / 5);
-      z =(z % 5);
+    if (num >= 10){
+      var x = parseInt(num / 10);
+      num = (num % 10);
     }
-    if (z >= 1){
-      var i = parseInt(z / 1);
-      z = (z % 1);
+    if (num >= 5){
+      var v = parseInt(num / 5);
+      num =(num % 5);
     }
-    if (z === 0){
-      return("M".repeat(m)) + ("D".repeat(d))+ ("C".repeat(c)) + ("L".repeat(l)) + ("X".repeat(x)) + ("V".repeat(v)) + ("I".repeat(i));
+    if (num >= 1){
+      var i = parseInt(num / 1);
+      num = (num % 1);
     }
-  }
-}
+    if (num === 0){
+      var stringResult = ("M".repeat(m)) + ("D".repeat(d))+ ("C".repeat(c)) + ("L".repeat(l)) + ("X".repeat(x)) + ("V".repeat(v)) + ("I".repeat(i));
+      // return stringResult;
+    }
+    if (stringResult.includes("DCCCC")); {
+    stringResult = stringResult.replace("DCCCC", "CM");
+    }
+    if (stringResult.includes("LXXXX")); {
+    stringResult = stringResult.replace("LXXXX", "XC");
+    }
+    if (stringResult.includes("VIIII"));{
+    stringResult = stringResult.replace("VIIII", "IX");
+    }
+    if (stringResult.includes("CCCC"));{
+      stringResult = stringResult.replace("CCCC", "CD");
+    }
+    if (stringResult.includes("IIII"));{
+      stringResult = stringResult.replace("IIII", "IV");
+    }
+    if (stringResult.includes("XXXX"));{
+      stringResult = stringResult.replace("XXXX", "XL");
+    }
+
+    return stringResult;
+    // var strArray = stringResult.split(" ");
+    //
+    // if (stringResult.includes("DCCC")){
+    //   var supersplice= stringResult.splice((stringResult.indexOf("DCCC")), 4, "CM");
+    //   return supersplice;
+
+    }
+
+
+
+
+
 
 
 
