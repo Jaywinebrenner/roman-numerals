37e6dc876b44824f9c70ddf81e61c177f562d759
Student <example@example.com>
2019-10-31 11:52:14 -0700

    add return for numerals

--- js/scripts.js
+++ js/scripts.js
@@ -4,38 +4,33 @@
 // var numD
 function convertatron(num) {
 
- for (var z = num; z > 0; z--){
-   if (z >= 10){
-     var x = parseInt(z / 10);
-     z = (z % 10);
-     console.log(x + " Xs");
-     if (z >= 5){
-       var v = parseInt(z / 5);
-       z =(z % 5);
-       console.log(v + " Vs");
-     }
-     if (z >= 1){
-         var i = parseInt(z / 1);
-         z = (z % 1);
-         console.log(i + " Is");
-       }
-   }
-   else if (z >= 5){
-     var v = parseInt(z / 5);
-     z =(z % 5);
-     console.log(v + " Vs");
-     if (z >= 1){
-       var i = parseInt(z / 1);
-       z = (z % 1);
-       console.log(i + " Is");
-     }
-   } else if (z >= 1){
-     var i = parseInt(z / 1);
-     z = (z % 1);
-     console.log(i + " Is");
-   }
- }
+  for (var z = num; z >= 0; z--){
+    if (z >= 100){
+      var c = parseInt(z / 100);
+      z = (z % 100);
+    }
+    if (z >= 50){
+      var l = parseInt(z / 50);
+      z = (z % 50);
+    }
+    if (z >= 10){
+      var x = parseInt(z / 10);
+      z = (z % 10);
+    }
+    if (z >= 5){
+      var v = parseInt(z / 5);
+      z =(z % 5);
+    }
+    if (z >= 1){
+      var i = parseInt(z / 1);
+      z = (z % 1);
+    }
+    if (z === 0){
+      return("C".repeat(c)) + ("L".repeat(l)) + ("X".repeat(x)) + ("V".repeat(v)) + ("I".repeat(i));
+    }
+  }
 }
+
 
 
 $(document).ready(function(){
