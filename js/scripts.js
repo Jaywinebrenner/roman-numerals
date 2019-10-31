53fac0e78b63d7c473d1b2e64adf60423a8653f2
Student <example@example.com>
2019-10-31 09:28:12 -0700

    create initial directory and files and some html and js

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+
+function convertatron(num) {
+  console.log("helo");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $("form#inputForm").submit(function(event){
+    event.preventDefault();
+
+    var number = $("input#inputField").val();
+    var convertatron(number);
+
+
+  });
+});
